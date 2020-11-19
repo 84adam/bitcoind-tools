ee3e2ad38dff2c662d49a9da62675f5879356023
84adam <84adam@users.noreply.github.com>
2020-11-19 20:50:48 +0000

    hard-code example path and drive

--- incoming.sh
+++ incoming.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# EDIT ME:
+LOG_PATH="/mnt/ext/bitcoin/debug.log"
+
+# EDIT ME:
+DRIVE="/dev/sda"
+
+# SCRIPT STARTS:
 echo "~ ~ ~ ~ TCP connections ~ ~ ~ ~"
 echo " "
 echo "Active Internet connections (w/o servers)"
@@ -15,7 +23,7 @@ echo " "
 echo "\$\$\$\$"
 echo " "
 # include path to bitcoind debug.log file below
-tail -n 200 /<PATH>/debug.log | grep "progress" | awk '{ print $1,$10,$5,$9,$4,$6,$7,$8,$11 }' | tail -n 3
+tail -n 200 $LOG_PATH | grep "progress" | awk '{ print $1,$10,$5,$9,$4,$6,$7,$8,$11 }' | tail -n 3
 echo " "
 echo "\$\$\$\$"
 echo " "
@@ -25,7 +33,7 @@ echo "\$\$\$\$"
 echo " "
 echo "Filesystem      Size  Used Avail Use% Mounted on"
 # indicate storage device by drive letter below
-df -h | grep /dev/sd<X>
+df -h | grep $DRIVE
 echo " "
 echo "\$\$\$\$"
 echo " "
